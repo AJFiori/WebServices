42a20a9ad1ff6645348f10d6bcdbe4ec42aeb87e
AJFiori <AJFioriiii@email.neit.edu>
2016-12-14 15:18:56 -0500

    Final

---
+++ Fiori_Final/FinalProject/FinalProject/listOfSources.js
@@ -0,0 +1,42 @@
+﻿
+
+
+    /*
+    var source = document.getElementById("newsSource");
+    var news = source.value.replace(" ", "-");
+    var news2 = news.toLowerCase();
+    */
+    var url = 'https://newsapi.org/v1/sources?&apiKey=cd52888677494997b8a81749a50a353d';
+    // Cross platform xmlhttprequest
+
+    // Create xmlhttprequest object
+    var xmlhttp = null;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+        //make sure that Browser supports overrideMimeType
+        if (typeof xmlhttp.overrideMimeType != 'undefined') { xmlhttp.overrideMimeType('text/xml'); }
+    } else if (window.ActiveXObject) {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+        alert('Perhaps your browser does not support xmlhttprequests?');
+    }
+
+    // Create an HTTP GET request
+    xmlhttp.open('GET', url, true);
+
+    // Set the callback function
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var myObj = JSON.parse(xmlhttp.responseText);
+
+            // $("#sourceTable").append("<thead><tr><th>List of News Sources To Use: </th></tr></thead>");
+            for (i = 0; i < myObj.sources.length; i++) {
+                $("#ddl").append("<option>" + myObj.sources[i].name + "</option>");               
+            }
+        } else {
+            // waiting for the call to complete
+        }
+    };
+
+    // Make the actual request
+    xmlhttp.send(null);
--- Fiori_Final/Fiori_Final/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace Fiori_Final.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
